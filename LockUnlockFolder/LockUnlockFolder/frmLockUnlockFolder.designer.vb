84ed34b964ab5bdb2e0d028c4e86a6e9ce6ac499
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2023-07-24 13:02:49 +0700

    Add files via upload

---
+++ LockUnlockFolder/LockUnlockFolder/frmLockUnlockFolder.designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLockUnlockFolder
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.btnFolderBrowse = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.Location = New System.Drawing.Point(0, 25)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(623, 567)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'btnFolderBrowse
+        '
+        Me.btnFolderBrowse.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnFolderBrowse.BackColor = System.Drawing.Color.ForestGreen
+        Me.btnFolderBrowse.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnFolderBrowse.FlatAppearance.BorderSize = 0
+        Me.btnFolderBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnFolderBrowse.ForeColor = System.Drawing.Color.White
+        Me.btnFolderBrowse.Location = New System.Drawing.Point(629, 261)
+        Me.btnFolderBrowse.Name = "btnFolderBrowse"
+        Me.btnFolderBrowse.Size = New System.Drawing.Size(112, 33)
+        Me.btnFolderBrowse.TabIndex = 0
+        Me.btnFolderBrowse.Text = "Select Folder"
+        Me.btnFolderBrowse.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.BackColor = System.Drawing.Color.Crimson
+        Me.btnExit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExit.FlatAppearance.BorderSize = 0
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(747, 261)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 33)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Location = New System.Drawing.Point(629, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(230, 230)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'frmLockUnlockFolder
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(863, 594)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnFolderBrowse)
+        Me.Controls.Add(Me.ListView1)
+        Me.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MinimumSize = New System.Drawing.Size(879, 633)
+        Me.Name = "frmLockUnlockFolder"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Locl/Unlock Folder with VB.NET - coDe bY: Thongkorn Tubtimkrob"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents btnFolderBrowse As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ LockUnlockFolder/LockUnlockFolder/frmLockUnlockFolder.vb
@@ -0,0 +1,116 @@
+﻿#Region "ABOUT"
+' / ------------------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: http://www.facebook.com/g2gnet (for Thailand)
+' / Facebook: http://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com
+' /
+' / Purpose: Lock/Unlock selected folder with code.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / ------------------------------------------------------------------------------------------
+#End Region
+
+Imports System.IO
+Imports System.Security.AccessControl
+
+Public Class frmLockUnlockFolder
+
+    Dim strPath As String = Application.StartupPath.ToLower.Replace("bin\debug", "").Replace("bin\release", "") & "Images\"
+
+    '// START HERE
+    Private Sub frmLockUnlockFolder_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        Label1.Text = ""
+        With PictureBox1
+            .SizeMode = PictureBoxSizeMode.StretchImage
+            .Image = Image.FromFile(strPath & "people.png") '\\ Initialize to show sample image.
+        End With
+        '// Start Lock or set permission with code.
+        Call LockFolder()
+    End Sub
+
+    '// Browse any folder.
+    Private Sub btnFolderBrowse_Click(sender As System.Object, e As System.EventArgs) Handles btnFolderBrowse.Click
+        Dim dlgFolderBrowse As New FolderBrowserDialog
+        dlgFolderBrowse.SelectedPath = strPath
+        '// If users select cancel then exit sub.
+        If dlgFolderBrowse.ShowDialog() = Windows.Forms.DialogResult.Cancel Then Exit Sub
+        '// 
+        If Microsoft.VisualBasic.Right(dlgFolderBrowse.SelectedPath, 1) <> "\" Then dlgFolderBrowse.SelectedPath = dlgFolderBrowse.SelectedPath & "\"
+        strPath = dlgFolderBrowse.SelectedPath
+        Try
+            '// Create ImageList dynamically.
+            Dim img As New ImageList
+            With img
+                .ImageSize = New Point(128, 128)
+                .ColorDepth = ColorDepth.Depth32Bit
+            End With
+
+            '// Before displaying the image, the folder must be Unlock first.
+            Call UnlockFolder()
+
+            Dim itemFolder As New List(Of ListViewItem)
+            For Each imgFile In Directory.GetFiles(dlgFolderBrowse.SelectedPath.ToString())
+                If imgFile.Contains(".png") Or imgFile.Contains(".jpg") Or imgFile.Contains(".bmp") Or imgFile.Contains(".gif") Then
+                    img.Images.Add(Image.FromFile(imgFile))
+                    itemFolder.Add(New ListViewItem(Path.GetFileName(imgFile)) With {.ImageIndex = img.Images.Count - 1})
+                End If
+            Next
+            With ListView1
+                .Items.Clear()
+                .View = View.LargeIcon
+                .LargeImageList = img
+                .Items.AddRange(itemFolder.ToArray())
+            End With
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Report Status", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+        '// When the image is displayed then Lock the folder again.
+        Call LockFolder()
+    End Sub
+
+    Private Sub ListView1_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ListView1.MouseClick
+        Dim item As ListViewItem = ListView1.HitTest(e.Location).Item
+        If item IsNot Nothing Then
+            Me.PictureBox1.Image = Image.FromFile(strPath & item.Text)
+            Label1.Text = "Image : " & strPath & item.Text
+        End If
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles ListView1.SelectedIndexChanged
+        'For Each item As ListViewItem In ListView1.SelectedItems
+        'MessageBox.Show(item.Text)
+        'Next
+    End Sub
+
+    ' / ------------------------------------------------------------------------------------------
+    '// Set Permission for lock/unlock folder with code.
+    Private Sub LockFolder()
+        Dim fs As FileSystemSecurity = File.GetAccessControl(strPath)
+        fs.AddAccessRule(New FileSystemAccessRule(Environment.UserName, FileSystemRights.FullControl, AccessControlType.Deny))
+        File.SetAccessControl(strPath, fs)
+    End Sub
+
+    Private Sub UnlockFolder()
+        Dim fs As FileSystemSecurity = File.GetAccessControl(strPath)
+        fs.RemoveAccessRule(New FileSystemAccessRule(Environment.UserName, FileSystemRights.FullControl, AccessControlType.Deny))
+        File.SetAccessControl(strPath, fs)
+    End Sub
+    ' / ------------------------------------------------------------------------------------------
+
+    Private Sub frmLockUnlockFolder_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Call UnlockFolder()    '// Unlock before the end of the program.
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
